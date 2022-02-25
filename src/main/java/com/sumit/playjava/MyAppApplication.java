49503bc0a90da02168dde6c5d618785ce97b824e
raju <raju.murugesan8454@gmail.com>
2022-02-25 12:55:31 +0530

    initial commit

---
+++ src/main/java/com/sumit/playjava/MyAppApplication.java
@@ -0,0 +1,37 @@
+package com.sumit.playjava;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/user")
+public class MyAppApplication {
+
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping("/app")
+	public String getApp() {
+		return "The port is "+port;
+	}
+	
+	@GetMapping("/country")
+	public String getConuntry() {
+		return "India";
+	}
+	
+	@GetMapping("/status")
+	public String getStatus() {
+		return "Active";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+
+}
